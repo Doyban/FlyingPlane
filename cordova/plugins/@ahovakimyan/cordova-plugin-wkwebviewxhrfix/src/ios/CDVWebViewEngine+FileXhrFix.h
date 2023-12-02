adf3d956c9ed68a41bc03ca565d61efcec383684
Daniel Danielecki <daniel.danielecki@mail.com>
2023-12-02 11:00:18 +0100

    Fix: assets not loading on localhost iPhone/Xcode

---
+++ cordova/plugins/@ahovakimyan/cordova-plugin-wkwebviewxhrfix/src/ios/CDVWebViewEngine+FileXhrFix.h
@@ -0,0 +1,14 @@
+//
+//  CDVWebViewEngine+FileXhrFix.h
+//
+//  Created by Ara Hovakimyan on 25/Aug/2020.
+//
+//
+
+#if __has_include("../../../../CordovaLib/Classes/Private/Plugins/CDVWebViewEngine/CDVWebViewEngine.h")
+#define EXIST_CDVWEBVIEWENGINE
+#import "../../../../CordovaLib/Classes/Private/Plugins/CDVWebViewEngine/CDVWebViewEngine.h"
+
+@interface CDVWebViewEngine (FileXhrFix)
+@end
+#endif
